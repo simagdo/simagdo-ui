710495a0d289865f81916e5c41ae3b625447d541
BuildTools <unconfigured@null.spigotmc.org>
2021-02-18 19:54:36 +0100

    Added Collapse.

---
+++ src/components/output/Collapse/Collapse.js
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+import PropTypes from 'prop-types';
+import './Collapse.sass';
+
+const Collapse = ({collapsed, children}) => {
+
+    const [isCollapsed, setIsCollapsed] = useState(collapsed);
+
+    return (
+        <div className="Collapse">
+            <button
+                className={`Collapse-Button ${isCollapsed ? 'Collapsed' : 'Expanded'}`}
+                onClick={() => {
+                    setIsCollapsed(!isCollapsed)
+                }}>
+                {isCollapsed ? 'Show' : 'Hide'} General Information
+            </button>
+            <div
+                className={`Collapse-Content ${isCollapsed ? 'Collapsed' : 'Expanded'}`}
+                aria-expanded={isCollapsed}>
+                {children}
+            </div>
+        </div>
+    );
+
+}
+
+Collapse.propTypes = {
+    collapsed: PropTypes.string,
+    children: PropTypes.object.isRequired
+};
+
+export default Collapse;
